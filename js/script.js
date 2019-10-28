b120374edf0fe3a1740fb510e1be238f6f71b88a
matuzalema <matuzalema@gmail.com>
2019-10-28 18:49:42 +0100

    Add mobile navigation

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
-const bar = document.getElementById("bar");
-const nav = document.querySelector(".ul-nav");
+const topNav = document.querySelector(".top-nav");
+const bar = document.querySelector('.fa-bars');
+const li = document.querySelectorAll('.top-nav>ul>li');
 
-bar.addEventListener('click', function(){
-    // nav.classList.add(".show");
+bar.addEventListener("click", () => {
+   topNav.classList.add('show');
+   bar.classList.add('hide');
+});
+
+li.forEach(item => {
+   item.addEventListener('click', () => {
+      topNav.classList.remove('show');
+      bar.classList.remove('hide');
+   })
 });
